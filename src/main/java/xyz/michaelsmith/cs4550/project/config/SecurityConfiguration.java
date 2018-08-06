22a3128bca825e84e7a0a4fdc516eadb91f9a8d8
Michael Smith <michael.smith@resonate.com>
2018-08-05 21:41:47 -0400

    fix security config

--- src/main/java/xyz/michaelsmith/cs4550/project/config/SecurityConfiguration.java
+++ src/main/java/xyz/michaelsmith/cs4550/project/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package xyz.michaelsmith.cs4550.project.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +9,11 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 
 @Configuration
-@Order(101)
+@EnableOAuth2Sso
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests().antMatchers("/").permitAll()
+            .anyRequest().authenticated();
     }
 }
