a002eb7179228afe239fa4394625b1caf687df18
Michael Smith <michael.smith@resonate.com>
2018-08-05 21:09:21 -0400

    return principal from current user endpoint

--- src/main/java/xyz/michaelsmith/cs4550/project/user/api/UserApiController.java
+++ src/main/java/xyz/michaelsmith/cs4550/project/user/api/UserApiController.java
@@ -1,11 +1,11 @@
 package xyz.michaelsmith.cs4550.project.user.api;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.michaelsmith.cs4550.project.user.data.entity.User;
 
 @RestController
 @RequestMapping("/api/me")
@@ -13,7 +13,7 @@ public class UserApiController {
     @RequestMapping(path = {"", "/"},
                     method = RequestMethod.GET,
                     produces = MediaType.APPLICATION_JSON_VALUE)
-    public Authentication getUserInfo() {
-        return SecurityContextHolder.getContext().getAuthentication();
+    public User getUserInfo() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
