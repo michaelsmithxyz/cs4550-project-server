af8964ca4c433842c4de0f54acdbb7a849ef5dfb
Michael Smith <michael.smith@resonate.com>
2018-08-08 20:35:22 -0400

    enable cors requests

---
+++ src/main/java/xyz/michaelsmith/cs4550/project/config/WebConfiguration.java
@@ -0,0 +1,13 @@
+package xyz.michaelsmith.cs4550.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
